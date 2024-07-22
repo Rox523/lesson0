02e692a583550f58471d362355e93b0b59320aa6
Rox523 <www.feart39@gmail.com>
2024-07-22 20:13:02 +0300

    Add files via upload

---
+++ datas1.py
@@ -0,0 +1,11 @@
+print("1st program")
+print(9 **0.5 * 5)
+
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print('3rd program')
+print(1234 % 1000 //10 + 5678 % 1000 //10)
+print('4th program')
+print(round(13.42%1*100) ==42) or (int(42.13) ==42)
+
